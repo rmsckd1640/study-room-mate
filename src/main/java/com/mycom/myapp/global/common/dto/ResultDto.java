fc66bb16b5e469c931291a7f7f7ff284c99374da
ddb27 <ddb2785@gmail.com>
2026-07-16 14:35:30 +0900

    refactor: ResultDto - @Data, @AllargsConstructor, @NoargsConstructor Add

--- src/main/java/com/mycom/myapp/global/common/dto/ResultDto.java
+++ src/main/java/com/mycom/myapp/global/common/dto/ResultDto.java
@@ -3,9 +3,15 @@ package com.mycom.myapp.global.common.dto;
 import java.util.List;
 import java.util.Map;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResultDto<T> {
 	
 	private int status;
