c75007b30715976c6140a96cf3e2dfc98d4befe6
rmsckd1640 <rmsckd1640@gmail.com>
2026-07-20 11:21:58 +0900

    feat: MemberService/MemberServiceImpl 회원가입 로직 구현

--- src/main/java/com/mycom/myapp/domain/member/service/MemberService.java
+++ src/main/java/com/mycom/myapp/domain/member/service/MemberService.java
@@ -1,5 +1,8 @@
 package com.mycom.myapp.domain.member.service;
 
-public interface MemberService {
+import com.mycom.myapp.domain.member.dto.MemberResponse;
+import com.mycom.myapp.domain.member.dto.SignupRequest;
 
+public interface MemberService {
+    MemberResponse signup(SignupRequest request);
 }
