4ab01d10aa4b9963772bf84a70b91188889d7bd8
rmsckd1640 <rmsckd1640@gmail.com>
2026-07-20 14:04:54 +0900

    feat: MemberController - 회원가입 API 추가

--- src/main/java/com/mycom/myapp/domain/member/controller/MemberController.java
+++ src/main/java/com/mycom/myapp/domain/member/controller/MemberController.java
@@ -1,5 +1,37 @@
 package com.mycom.myapp.domain.member.controller;
 
+import com.mycom.myapp.domain.member.dto.MemberResponse;
+import com.mycom.myapp.domain.member.dto.SignupRequest;
+import com.mycom.myapp.domain.member.service.MemberService;
+import com.mycom.myapp.global.common.dto.ResultDto;
+
+import jakarta.validation.Valid;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
 public class MemberController {
 
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity<ResultDto<MemberResponse>> signup(@Valid @RequestBody SignupRequest request) {
+        MemberResponse response = memberService.signup(request);
+
+        ResultDto<MemberResponse> result = ResultDto.<MemberResponse>builder()
+                .message("회원가입이 완료되었습니다.")
+                .data(response)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
 }
