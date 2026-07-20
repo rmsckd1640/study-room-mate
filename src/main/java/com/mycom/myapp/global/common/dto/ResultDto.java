71754aa9b8b549fe60330c5af95d622229e52d94
kl35805 <kl35805@gmail.com>
2026-07-20 11:41:40 +0900

    refactor : ResultDto 수정

--- src/main/java/com/mycom/myapp/global/common/dto/ResultDto.java
+++ src/main/java/com/mycom/myapp/global/common/dto/ResultDto.java
@@ -1,7 +1,5 @@
 package com.mycom.myapp.global.common.dto;
 
-import org.springframework.http.HttpStatus;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,10 +10,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ResultDto<T> {
-	
-	private String result;
-	private HttpStatus status;
 	private String message;
 	private T data;
-	
 }
