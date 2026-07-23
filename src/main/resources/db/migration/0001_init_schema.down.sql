c3bb86df0798ce9c908ede075142bb267270469d
rmsckd1640 <rmsckd1640@gmail.com>
2026-07-23 10:14:16 +0900

    fix: 스키마 오류 수정

--- src/main/resources/db/migration/0001_init_schema.down.sql
+++ src/main/resources/db/migration/0001_init_schema.down.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS `payment`;
 DROP TABLE IF EXISTS `reservation`;
 DROP TABLE IF EXISTS `wishlist`;
 DROP TABLE IF EXISTS `review`;
+DROP TABLE IF EXISTS `password_reset_token`;
 DROP TABLE IF EXISTS `refresh_token`;
 DROP TABLE IF EXISTS `room`;
 DROP TABLE IF EXISTS `member`;
